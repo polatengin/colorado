d4a08e829b8ebeb2cefdf2173164e17d8605144f
Engin Polat <polatengin@hotmail.com>
2023-03-03 19:13:46 +0000

    rendering maps on screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,21 @@
 import styles from './page.module.css'
 
 import { Loader } from "@googlemaps/js-api-loader"
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    const loader = new Loader({
+      apiKey: "",
+      version: "weekly",
+    });
+
+    loader.load().then(() => {
+        const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+          zoom: 16,
+        });
+  });
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -12,6 +26,7 @@ export default function Home() {
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
+      <div id="map" style={{width: "100%", minHeight: "600px"}}></div>
     </main>
   )
 }
