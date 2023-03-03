3c7879cfe8280c48809397aced3589876faa0a1a
Engin Polat <polatengin@hotmail.com>
2023-03-03 22:43:33 +0000

    simplifying dom elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,16 @@
-"use client";
+import dynamic from 'next/dynamic';
 
-import styles from './page.module.css'
-
-import { useEffect } from 'react';
+const Maps = dynamic(() => import("../components/maps"), {
+  ssr: false,
+});
 
 export default function Home() {
-  useEffect(() => {
-  });
-
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-      </div>
-      <div id="map" style={{width: "100%", minHeight: "600px"}}></div>
+    <div>
+      <Maps />
       <div id="address_form">
         Address: <input type="text" />
       </div>
-    </main>
+    </div>
   )
 }
